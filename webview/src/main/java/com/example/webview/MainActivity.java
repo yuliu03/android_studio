8196d9fab163ea4c1f38c852ca05d3bb979ac69f
刘愉 <liuyu1024@hotmail.com>
2020-02-29 22:03:53 +0800

    added webview

---
+++ webview/src/main/java/com/example/webview/MainActivity.java
@@ -0,0 +1,34 @@
+package com.example.webview;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        //获得控件
+         WebView webView = (WebView) findViewById(R.id.web);
+         //访问网页
+//         webView.loadUrl("https://m.baidu.com");
+        webView.loadUrl("http://192.168.0.102:8081/");
+        WebSettings webSettings = webView.getSettings();
+        webSettings.setJavaScriptEnabled(true);
+        //系统默认会通过手机浏览器打开网页，为了能够直接通过WebView显示网页，则必须设置
+        webView.setWebViewClient(new WebViewClient(){
+            @Override
+            public boolean shouldOverrideUrlLoading(WebView view, String url) {
+                //使用WebView加载显示url
+                view.loadUrl(url);
+                //返回true
+                return true;
+            }
+        });
+    }
+}
